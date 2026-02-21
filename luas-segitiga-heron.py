95f0a75dd49c1d6d0fb56f842aeaf27b47be4310
Logarythms1914 <parmenas52@gmail.com>
2026-02-21 13:47:13 +0800

    Update 1 luas-segitiga-heron.py
    Menambahkan perhitungan luas segigita pakai rumus Heron

--- luas-segitiga-heron.py
+++ luas-segitiga-heron.py
@@ -5,3 +5,6 @@ def segitiga1():
     s = (sisi_1 + sisi_2 + sisi_3)/2  # setengah keliling segitiga
     return (s * (s - sisi_1) * (s - sisi_2) * (s - sisi_3))**0.5
 
+if nama_bangun_datar == 'segitiga':
+    luas = segitiga1()
+    print(f'Luas segitiganya adalah {luas}')
