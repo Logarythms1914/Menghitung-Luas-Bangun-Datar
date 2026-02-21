d4c5c1752c5618e77c0e60305ce7f27e90dce133
Logarythms1914 <parmenas52@gmail.com>
2026-02-21 13:00:39 +0800

    Update 1 Kode-Luas-Bangun-Datar.py
    Menambahkan perhitungan luas lingkaran

--- Kode-Luas-Bangun-Datar.py
+++ Kode-Luas-Bangun-Datar.py
@@ -1,12 +1,23 @@
+from math import pi
+
 def persegi():
     sisi = int(input('Masukkan nilai sisi: '))
     return sisi**2
 
+
+def lingkaran():
+    jari_jari = int(input('Masukkan nilai jari-jari: '))
+    return pi*jari_jari**2
+
 nama_bangun_datar = input('Masukkan nama bangun datar: ')
 nama_bangun_datar = nama_bangun_datar.lower()
 
-if nama_bangun_datar != 'persegi':
+if nama_bangun_datar not in ('persegi','lingkaran'):
     print(f'Maaf bangun datar yang kamu cari belum ada rumusnya di program ini')
 else:
-    luas = persegi()
-    print(f'Luas persegi adalah {luas}')
+    if nama_bangun_datar == 'persegi':
+        luas = persegi()
+        print(f'Luas perseginya adalah {luas}')
+    elif nama_bangun_datar == 'lingkaran':
+        luas = lingkaran()
+        print(f'Luas lingkarannya adalah {round(luas,2)}')
