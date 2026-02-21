286544865889ca1a2a061e0c57c079b61563ab99
Logarythms1914 <parmenas52@gmail.com>
2026-02-21 13:07:38 +0800

    Update 2 Kode-Luas-Bangun-Datar.py
    - Mengganti perhitungan luas persegi dengan luas persegi panjang karena lebih umum.
    
    - Mengubah semua tipe data yang diinput untuk menghitung bangun datar menjadi tipe data float.

--- Kode-Luas-Bangun-Datar.py
+++ Kode-Luas-Bangun-Datar.py
@@ -1,23 +1,24 @@
 from math import pi
 
-def persegi():
-    sisi = int(input('Masukkan nilai sisi: '))
-    return sisi**2
+def persegi_panjang():
+    panjang = float(input('Masukkan nilai panjang: '))
+    lebar = float(input('Masukkan nilai lebar: '))
+    return panjang * lebar
 
 
 def lingkaran():
-    jari_jari = int(input('Masukkan nilai jari-jari: '))
-    return pi*jari_jari**2
+    jari_jari = float(input('Masukkan nilai jari-jari: '))
+    return pi * jari_jari ** 2
 
 nama_bangun_datar = input('Masukkan nama bangun datar: ')
 nama_bangun_datar = nama_bangun_datar.lower()
 
-if nama_bangun_datar not in ('persegi','lingkaran'):
+if nama_bangun_datar not in ('persegi','lingkaran','persegi panjang'):
     print(f'Maaf bangun datar yang kamu cari belum ada rumusnya di program ini')
 else:
-    if nama_bangun_datar == 'persegi':
-        luas = persegi()
-        print(f'Luas perseginya adalah {luas}')
+    if nama_bangun_datar == 'persegi' or nama_bangun_datar == 'persegi panjang':
+        luas = persegi_panjang()
+        print(f'Luas bangun datarnya adalah {luas}')
     elif nama_bangun_datar == 'lingkaran':
         luas = lingkaran()
-        print(f'Luas lingkarannya adalah {round(luas,2)}')
+        print(f'Luas lingkarannya adalah {luas}')
