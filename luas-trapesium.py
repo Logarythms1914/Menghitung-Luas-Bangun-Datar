e3efa82e1b43ab79d86bf69b0d420e8e909270fc
Logarythms1914 <parmenas52@gmail.com>
2026-02-23 02:26:59 +0800

    Mengubah sedikit file luas-segitiga-heron.py dan menghapus file luas-trapesium.py dan isi file luas-trapesium.py dimasukkan ke file Kode-Luas-Bangun-Datar.py

--- luas-trapesium.py
+++
@@ -1,5 +0,0 @@
-def trapesium():
-    sisi_1 = float(input('Masukkan nilai sisi 1: '))
-    sisi_2 = float(input('Masukkan nilai sisi 2: '))
-    tinggi = float(input('Masukkan nilai tinggi trapesium: '))
-    return 0.5 * (sisi_1 + sisi_2) * tinggi 
