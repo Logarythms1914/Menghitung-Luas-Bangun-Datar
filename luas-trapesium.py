60ba1b281cfff01a42f2d14ac60143d22efe80d4
Logarythms1914 <parmenas52@gmail.com>
2026-02-23 02:08:16 +0800

    Menambahkan file luas trapesium

---
+++ luas-trapesium.py
@@ -0,0 +1,5 @@
+def trapesium():
+    sisi_1 = float(input('Masukkan nilai sisi 1: '))
+    sisi_2 = float(input('Masukkan nilai sisi 2: '))
+    tinggi = float(input('Masukkan nilai tinggi trapesium: '))
+    return 0.5 * (sisi_1 + sisi_2) * tinggi 
